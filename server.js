d3521e1f2290cb6e2694e6aea31f8a0869fc8bec
Ben Kersey <kersey24@ad.unc.edu>
2023-04-26 15:11:40 -0400

    added ;

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 #!/usr/bin/env node
 
-import {rps, rpsls} from "./lib/rpsls.js"
-import minimist from 'minimist'
-import express from 'express'
+import {rps, rpsls} from "./lib/rpsls.js";
+import minimist from 'minimist';
+import express from 'express';
 
 const args = minimist(process.argv.slice(2)); 
-const port = args.port || 5000;
+const port = args["port"] || 5000;
 const app = express();
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
